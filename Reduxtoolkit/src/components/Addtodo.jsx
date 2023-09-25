f6e935de7e9d785e9504d93c99613251aa837fd9
abhay395 <140682257+abhay395@users.noreply.github.com>
2023-09-25 16:41:22 +0530

    complte sir assignment

--- Reduxtoolkit/src/components/Addtodo.jsx
+++ Reduxtoolkit/src/components/Addtodo.jsx
@@ -1,29 +1,55 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTodo } from "../feature/todo/todoSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch ,useSelector} from "react-redux";
+import { addTodo, updateTodo} from "../feature/todo/todoSlice";
 
 function Addtodo() {
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
-  const addTodoHandler = (e) => {
-    e.preventDefault();
+  // updating
+  const todos = useSelector((state) => state.todos);
+  const updatingTodoid=useSelector((state)=>state.updatetodoid)
+  const updatingTodo=todos.filter((todo)=>todo.id===updatingTodoid)
+  const updatingtext=updatingTodo.length ? updatingTodo[0].text:''
+
+  const updatingmode=updatingTodo.length ? updatingTodo[0].updateTodoMode:false
+  const updateTododisplay=()=>{
+   updatingmode?setInput(updatingtext):setInput('')
+  }
+  const updatetoodoHandler=()=>{
+    dispatch(updateTodo([updatingTodoid,input]))
+  }
+  // const buttonHandler=(e)=>{
+  //   e.preventDefault();
+  //   updatingmode?updatetoodoHandler():addTodoHandler()
+    
+  // }
+  const addTodoHandler = () => {
+    // e.preventDefault();
     dispatch(addTodo(input));
     setInput("");
   };
+
+  useEffect(()=>{
+    updateTododisplay()
+  },[updatingmode])
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+    <form onSubmit={(e)=>{
+      e.preventDefault()
+      updatingmode?updatetoodoHandler():addTodoHandler()
+    }} className="space-x-3 mt-12">
       <input
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-out "
         type="text"
         placeholder="Enter a Todo"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        
       />
       <button
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >
-        Add Todo
+        {updatingmode?'Update':'Add'}
       </button>
     </form>
   );
--- Reduxtoolkit/src/feature/todo/todoSlice.js
+++ Reduxtoolkit/src/feature/todo/todoSlice.js
@@ -1,9 +1,11 @@
 import {createSlice,nanoid} from '@reduxjs/toolkit'
 
 const initialState={
+    updatetodoid:0,
     todos:[{
        text:'',
-       id:1 
+       id:1 ,
+       updateTodoMode:false
     }]
 }
 
@@ -17,8 +19,26 @@ export const todoSlice=createSlice({
         },
         removeTodo:(state,action)=>{
             state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
+            
+            
+        },
+        toggleTodomode:(state,action)=>{
+            state.todos.map((todo)=>{
+                if(todo.id===action.payload){
+                    todo.updateTodoMode=!todo.updateTodoMode
+                }
+                state.updatetodoid=action.payload
+            })
+        },
+        updateTodo:(state,action)=>{
+            state.todos.map((todo)=>{
+                if(todo.id===action.payload[0]){
+                    todo.text=action.payload[1]
+                    todo.updateTodoMode=!todo.updateTodoMode
+                }
+            })
         }
     }
 })
- export const {addTodo,removeTodo}=todoSlice.actions
+ export const {addTodo,removeTodo,toggleTodomode,updateTodo}=todoSlice.actions
  export default todoSlice.reducer
